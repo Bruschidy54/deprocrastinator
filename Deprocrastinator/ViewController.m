9b8335e17074fbf0e5300274f32c37998bfe3361
Dylan Bruschi <dylanbruschi54@gmail.com>
2016-03-21 15:13:04 -0500

    Swipe fix

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UITableViewDelegate,UITableViewDataSource>
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource, UIGestureRecognizerDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UITextField *textFieldOutlet;
 @property (weak, nonatomic) IBOutlet UIButton *editButton;
@@ -42,12 +42,12 @@
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     cell.textLabel.textColor = [UIColor greenColor];
-    if ([self.editButton.currentTitle isEqualToString: @"Done"]) {
-        cell.textLabel.textColor = [UIColor blackColor];
-        [self.tasks removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]withRowAnimation:YES];
-        [self.tableView reloadData];
-    }
+//    if ([self.editButton.currentTitle isEqualToString: @"Done"]) {
+//        cell.textLabel.textColor = [UIColor blackColor];
+//        [self.tasks removeObjectAtIndex:indexPath.row];
+//        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]withRowAnimation:YES];
+//        [self.tableView reloadData];
+//    }
     
 }
 
@@ -90,9 +90,17 @@ if ([self.editButton.currentTitle isEqualToString:@"Done"]) {
       
 }
 - (IBAction)onSwipeGesture:(UISwipeGestureRecognizer *)sender {
-    sender
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        CGPoint gestureLocation = [sender locationInView:self.tableView];
+        NSIndexPath *swipedIndexPath = [self.tableView indexPathForRowAtPoint:gestureLocation];
+        //found IndexPath for swiped cell. Now we can do anything what we need.
+        //In my case it's cell reloading with new image in UIImageView.
+       UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:swipedIndexPath];
+         cell.textLabel.textColor = [UIColor redColor];
+        [self.tableView reloadData];
     
     
+}
 }
 
 
